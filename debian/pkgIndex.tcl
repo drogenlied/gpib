4f70f91eff1e6604a474e4bdd1adb5a23bd46434
Jan Binder <jan-git@nlogn.org>
2023-06-09 18:21:31 +0200

    tcl packaging

---
+++ debian/pkgIndex.tcl
@@ -0,0 +1,12 @@
+if {[package vsatisfies [package present Tcl] 8.5]} {
+	package ifneeded gpib 4.3.5 [list apply {{dir} {
+		load [file join $dir libgpib_tcl.so] gpib
+
+		set gpibTclInitScript [file join $dir gpib.tcl]
+		if {[file exists $gpibTclInitScript]} {
+			source $gpibTclInitScript
+		}
+	}} $dir]
+} elseif {[package vsatisfies [package present Tcl] 8.4]} {
+	package ifneeded gpib 4.3.5 [list load [file join $dir libgpib_tcl.so] gpib]
+}
