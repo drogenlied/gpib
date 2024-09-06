609b0fc6b12f0b97e1c5c6c4b2daf0d98e15c22b
Jan Binder <jan-git@nlogn.org>
2024-09-06 12:09:55 +0200

    packaging updates

--- debian/pkgIndex.tcl
+++ debian/pkgIndex.tcl
@@ -1,5 +1,5 @@
 if {[package vsatisfies [package present Tcl] 8.5]} {
-	package ifneeded gpib 4.3.5 [list apply {{dir} {
+	package ifneeded gpib 4.3.7 [list apply {{dir} {
 		load [file join $dir libgpib_tcl.so] gpib
 
 		set gpibTclInitScript [file join $dir gpib.tcl]
@@ -8,5 +8,5 @@ if {[package vsatisfies [package present Tcl] 8.5]} {
 		}
 	}} $dir]
 } elseif {[package vsatisfies [package present Tcl] 8.4]} {
-	package ifneeded gpib 4.3.5 [list load [file join $dir libgpib_tcl.so] gpib]
+	package ifneeded gpib 4.3.7 [list load [file join $dir libgpib_tcl.so] gpib]
 }
